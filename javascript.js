c6c0e8837a8a2b12e7d4b7841251dbb61ac8d914
Connor <cfech44@gmail.com>
2020-02-08 12:17:56 -0500

    Major updates to index.html, addding repsponsiveness for mobile, addition of new nav bar and 2nd page with sacko formula and explanation, also experimenting with tickers

--- javascript.js
+++ javascript.js
@@ -1,25 +1,26 @@
-// alert("test")
 
-var poem = "Andreas(2017) Alec(2018) Connor()2019";
-var words = poem.split(" ");
+var poem = "Fantasy Champions: Andreas(2017) - MVP:Alvin Kamara, Sacko     - Dan(2017),  Fantasy Champions:Alec(2018) - MVP: Patrick Mahomes, Sacko -  Dan(2018)(x2), Fantasy Champions:Connor(2019) - MVP:Aaron Jones,Sacko Ethan(2019)";
+var words = poem.split(",");
 
 var mainEl = document.getElementById("main");
-var readEl = document.getElementById("read");
+var readEl = document.getElementById("array");
 var timerEl = document.getElementById("countdown");
 var bodyEl = document.createElement("div");
 
-var i = 0;
+var millisecondsPerWord = 1500
+var i = 0
+
 function speedRead() {
-    mainEl.append(bodyEl);
-  
-    var poemInterval = setInterval(function() {
-      if (words[i] === undefined) {
-        clearInterval(poemInterval);
-      } else {
-        mainEl.textContent = words[i];
-        i++;
-      }
-  
-    }, millisecondsPerWord);
-  }
+  mainEl.append(bodyEl);
+
+  setInterval(function() {
+    if (words) {
+      mainEl.textContent= words[i];i++
+      
+    } 
+  }, millisecondsPerWord);
+}
+
+
+speedRead()
   
