2fadf95796e11c8872b3922b28081bcf3c11ec60
Connor <cfech44@gmail.com>
2020-02-09 02:25:29 -0500

    Adding a 3rd page for rules, as well as updating the framework of all pages

--- javascript.js
+++ javascript.js
@@ -1,14 +1,21 @@
 
-var poem = "Fantasy Champions: Andreas(2017) - MVP:Alvin Kamara, Sacko     - Dan(2017),  Fantasy Champions:Alec(2018) - MVP: Patrick Mahomes, Sacko -  Dan(2018)(x2), Fantasy Champions:Connor(2019) - MVP:Aaron Jones,Sacko Ethan(2019)";
+var poem = "Fantasy Champions: Andreas(2017) - MVP:Alvin Kamara, Sacko     - Dan(2017),  Fantasy Champions:Alec(2018) - MVP: Patrick Mahomes, Sacko -  Dan(2018)(x2), Fantasy Champions:Connor(2019) - MVP:Aaron Jones,Sacko Ethan(2019),Fantasy Champions: Andreas(2017) - MVP:Alvin Kamara, Sacko     - Dan(2017),  Fantasy Champions:Alec(2018) - MVP: Patrick Mahomes, Sacko -  Dan(2018)(x2), Fantasy Champions:Connor(2019) - MVP:Aaron Jones,Sacko Ethan(2019),Fantasy Champions: Andreas(2017) - MVP:Alvin Kamara, Sacko     - Dan(2017),  Fantasy Champions:Alec(2018) - MVP: Patrick Mahomes, Sacko -  Dan(2018)(x2), Fantasy Champions:Connor(2019) - MVP:Aaron Jones,Sacko Ethan(2019),Fantasy Champions: Andreas(2017) - MVP:Alvin Kamara, Sacko     - Dan(2017),  Fantasy Champions:Alec(2018) - MVP: Patrick Mahomes, Sacko -  Dan(2018)(x2), Fantasy Champions:Connor(2019) - MVP:Aaron Jones,Sacko Ethan(2019),Fantasy Champions: Andreas(2017) - MVP:Alvin Kamara, Sacko     - Dan(2017),  Fantasy Champions:Alec(2018) - MVP: Patrick Mahomes, Sacko -  Dan(2018)(x2), Fantasy Champions:Connor(2019) - MVP:Aaron Jones,Sacko Ethan(2019),Fantasy Champions: Andreas(2017) - MVP:Alvin Kamara, Sacko     - Dan(2017),  Fantasy Champions:Alec(2018) - MVP: Patrick Mahomes, Sacko -  Dan(2018)(x2), Fantasy Champions:Connor(2019) - MVP:Aaron Jones,Sacko Ethan(2019)";
 var words = poem.split(",");
 
 var mainEl = document.getElementById("main");
 var readEl = document.getElementById("array");
 var timerEl = document.getElementById("countdown");
 var bodyEl = document.createElement("div");
+var container1 = document.getElementById("container1")
 
-var millisecondsPerWord = 1500
+var millisecondsPerWord = "1500"
 var i = 0
+
+// for (i=0;i<100;i++){
+//   console.log(i)
+//   speedRead()
+// }
+
 
 function speedRead() {
   mainEl.append(bodyEl);
@@ -21,6 +28,16 @@ function speedRead() {
   }, millisecondsPerWord);
 }
 
-
 speedRead()
-  
+
+
+
+
+// function addBorder(){
+//   cells.classList.add("hide")
+//   console.log(cells)
+// }
+
+// var cells = document.getElementsByClassName("tg-0lax")
+
+// addBorder()
