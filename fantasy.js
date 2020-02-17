e05fe6b14e53103ccf19902d986308c26dd32a7d
Connor <cfech44@gmail.com>
2020-02-17 16:56:34 -0500

    adding local storage list to rules.html

--- fantasy.js
+++ fantasy.js
@@ -1,7 +1,7 @@
 
 var ideas
 var prevIdeas =[]
-
+var newRulesArray = []
 
 
 var datetime = null,
@@ -9,6 +9,23 @@ var datetime = null,
 
 
 // Setting date on home page 
+retrieveStorage()
+function retrieveStorage() {
+  if(localStorage.getItem("NewRules") !== null){
+  prevRules = localStorage.getItem("NewRules").split(",")
+  console.log("TCL: retrieveStorage -> prevRules", prevRules)
+  console.log()
+
+  newRulesArray.push(prevRules)
+  console.log("TCL: retrieveStorage -> newRulesArray", newRulesArray)
+
+  for (i = 0; i < prevRules.length && i < 10; i++) {
+      let searchItem = $("<li>")
+      searchItem.text(prevRules[i])
+      $("#newRulesList").prepend(searchItem)
+  }
+}
+}
 
 var update = function () {
     date = moment(new Date())
@@ -22,21 +39,33 @@ $(document).ready(function(){
 });
 
 
-var newRulesArray = []
+
 
 $("#add-rule").on("click", function () {
+  $("#newRulesList").empty()
   event.preventDefault()
 let newRule = $("#rule-input").val()
 console.log(newRule)
 newRulesArray.push(newRule)
+localStorage.setItem("NewRules", newRulesArray)
 console.log(newRulesArray)
 
   console.log(newRulesArray)
-  
-for (i = 0; i < newRulesArray.length; i++)
+
+for (i = 0; i < newRulesArray.length; i++){
   var newRuleLi = $("<li>").addClass("newRule")
   newRuleLi.text(newRulesArray[i])
   console.log(newRulesArray)
   console.log(newRuleLi)
   $("#newRulesList").append(newRuleLi)
+}
 })
+
+
+
+
+
+
+
+
+
